e056dde73e8e3b4c7548be421b76f97429148a69
andresombra <andresombra.net@gmail.com>
2023-09-25 17:38:00 -0300

    Incluido Produto e Categoria

--- Estoque.Domain/Entidades/Categoria.cs
+++ Estoque.Domain/Entidades/Categoria.cs
@@ -6,11 +6,9 @@ using System.Threading.Tasks;
 
 namespace Estoque.Domain.Entidades
 {
-    public class Produto
+    public class Categoria
     {
-        public int ProdutoId { get; set; }
-        public string Descricao { get; set; } = string.Empty;   
-        public decimal Valor { get; set; }
         public int CategoriaId { get; set; }
+        public string Nome { get; set; }
     }
 }
